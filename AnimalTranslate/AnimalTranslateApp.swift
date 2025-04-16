8bfd28a37efe59bd45cd79fe13b487eabd8216ae
Artem <dogmaster31072003@gmail.com>
2025-04-16 17:45:26 +0300

    Initial Commit

---
+++ AnimalTranslate/AnimalTranslateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalTranslateApp.swift
+//  AnimalTranslate
+//
+//  Created by Melkor on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimalTranslateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimalTranslate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnimalTranslate
+//
+//  Created by Melkor on 4/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
